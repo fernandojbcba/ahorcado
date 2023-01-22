06ca2299890f71ce95b0a89c77f8d3ee9193c6fa
fernandojbcba <benavidez.fernando@hotmail.com>
2023-01-22 00:52:26 -0300

    casi listo

--- js/scripts.js
+++ js/scripts.js
@@ -15,7 +15,7 @@ let guiones_text=document.querySelector("#guiones");
 let errados_text=document.querySelector("#errados");
 let winloser_text=document.querySelector("#winloser");
 
-let nombres=["ORACLE","ALURA","LATAM","ONE"];
+let nombres=["ORACLE","ALURA","LATAM","ONE","ARBOL","CASA","TORO","PERRO","GATO","FERROCARRIL","SALAME","DELFIN","LEON"];
 let palabraAleatoria_String="";
 let palabraAleatoria_Length = 0;
 let palabraAleatoria_Array =[];
@@ -33,7 +33,7 @@ boton_cancelar.addEventListener("click", () => {inicio()});
 boton_desistir.addEventListener("click", () => {inicio()});
 juego_nuevo.addEventListener("click", () => {crear_juego()});
 juego_nuevo_nuevo.addEventListener("click", () => {crear_juego()});
-
+boton_agregar_palabra_nueva.addEventListener("click",()=>{funcion_agregar_palabra()});
 
 
 function crear_juego(){
@@ -51,30 +51,25 @@ function crear_juego(){
     palabraAleatoria_Length=palabraAleatoria_String.length;
     palabraAleatoria_Array=palabraAleatoria_String.split("");
     generador_guion();
-    
-    
-
-    console.log(errados_text);
-    console.log(palabra_guiones);
-    console.log(palabraAleatoria_Array);
+if (findejuego==false){
     window.addEventListener('keydown', captura_letra);
-    console.log(palabraAleatoria_String);
+}
+ 
 
-
-
-    
 }
 function agregar_palabra(){
     seccion_inicio.style.display = "none";
     seccion_juego.style.display = "none";
     seccion_nueva_palabra.style.display = "flex";
     seccion_gano_perdio.style.display="none"
+    findejuego=true;
 }
 function inicio(){
     seccion_inicio.style.display = "flex";
     seccion_juego.style.display = "none";
     seccion_nueva_palabra.style.display = "none";
     seccion_gano_perdio.style.display="none"
+    findejuego=true;
 }
 
 function palabra_aleatoria(){
@@ -86,10 +81,11 @@ function captura_letra(event){
 letra_ingresada = event.key;
 letra=letra_ingresada.toUpperCase();
 console.log(letra);
-if (verificacionCaracter.test(letra)){
-    verifica_letra_en_palabra();
-}
-    
+    if ((letra!="META") && (letra!="SHIFT") && (letra!="ALT") && (letra!="CONTROL") && (letra!="CAPSLOCK") && (letra!="ENTER") && (letra!="BACKSPACE")){
+        if (verificacionCaracter.test(letra)){
+            verifica_letra_en_palabra();
+        }
+    }   
 }
 function verifica_letra_en_palabra(){
 if (!letra_ingresadas.includes(letra)){
@@ -137,7 +133,7 @@ winloser.textContent = "ganaste"
 
 }
 function erradas(){
-   if(contadorerrados==6){
+   if(contadorerrados==6 && (findejuego==false)){
     console.log("perdiste");
     document.getElementById("gano_perdio_img").src="./img/loser.png";
     seccion_gano_perdio.style.display="flex"
@@ -157,4 +153,25 @@ function reset(){
     cant_acertadas=0;
     contadorerrados=0;
     palabra_guiones=[];
+}
+
+
+    
+
+function funcion_agregar_palabra(){
+    console.log("vacio")
+    let mensaje = document.querySelector("#nueva_palabra").value;
+
+    if(mensaje!="" && !/[A-Z]/g.test((mensaje) && !/[á-ú]/g.test(mensaje) && mensaje.trim().length)){
+     let texto = mensaje.toUpperCase();
+     console.log(texto);
+     if ((nombres.includes(texto))!=true){
+        nombres.push(texto);
+
+
+     }
+    }
+
+    else alert("Por favor, ingrese un mensaje en minúsculas y sin acentos")
+    console.log(nombres);
 }
